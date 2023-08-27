d8f19b63321a5f409fe2032d474198291b236314
2faceid <liorfeldman87@gmail.com>
2023-08-27 20:07:38 +0300

    new

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+
+const app = express();
+
+app.use(express.urlencoded({extended:false}))
+app.use(express.json())
+
+const port = 3001 ;
+
+app.listen(port, () => {
+    console.log(" Server is running ")
+})
